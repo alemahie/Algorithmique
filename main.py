a56bbb687c5100f9abe5e74bd5b2f2a6b9bc18aa
alemahie <32846956+alemahie@users.noreply.github.com>
2018-10-28 11:47:54 +0100

    Arbre aléatoire

--- main.py
+++ main.py
@@ -8,31 +8,40 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import random
 
+#########################################################################################################
+
 def hierarchy_pos(G, root, width=1., vert_gap = 0.2, vert_loc = 0, xcenter = 0.5, pos = None, parent = None):
-	if pos == None:
-		#pos = {G.node[root]['weight']:(xcenter,vert_loc)}
-		pos = {root:(xcenter,vert_loc)}
-	else:
-		pos[root] = (xcenter, vert_loc)
-	neighbors = list(G.neighbors(root)) 
-	if parent != None:   #this should be removed for directed graphs.
-		neighbors.remove(parent)  #if directed, then parent not in neighbors.
-	if len(neighbors)!=0:
-		dx = width/len(neighbors) 
-		nextx = xcenter - width/2 - dx/2
-		for neighbor in neighbors:
-			nextx += dx
-			pos = hierarchy_pos(G,neighbor, width = dx, vert_gap = vert_gap, vert_loc = vert_loc-vert_gap, xcenter=nextx, pos=pos, parent = root)						
-	return pos
+    if pos == None:
+        #pos = {G.node[root]['weight']:(xcenter,vert_loc)}
+        pos = {root:(xcenter,vert_loc)}
+    else:
+        pos[root] = (xcenter, vert_loc)
+    neighbors = list(G.neighbors(root)) 
+    if parent != None:   #this should be removed for directed graphs.
+        neighbors.remove(parent)  #if directed, then parent not in neighbors.
+    if len(neighbors)!=0:
+        dx = width/len(neighbors) 
+        nextx = xcenter - width/2 - dx/2
+        for neighbor in neighbors:
+            nextx += dx
+            pos = hierarchy_pos(G,neighbor, width = dx, vert_gap = vert_gap, vert_loc = vert_loc-vert_gap, xcenter=nextx, pos=pos, parent = root)                       
+    return pos
 
 
 
 def gen_sommet():
-	n_sommets = random.randint(10,15) #génère entre 10 et 15 sommets
-	for i in range(n_sommets):
-		pass
+    G=nx.Graph()
+    n_sommets = random.randint(10,15) #génère entre 10 et 15 sommets
+    for i in range(n_sommets):
+        G.add_node(i+1, weight = random.randint(-10,10))    #Créé un noeud aléatoire
+        if i != 0:
+            G.add_edge(random.randint(1, i), i+1)   #Relie ce nouveau noeud (sauf le premier), aléatoirement parmis les précédants
+    
+    return G
 
+##########################################################################################################
 
+"""
 G=nx.Graph()
 G.add_node(1, weight=-7)
 G.add_node(2, weight=4)
@@ -51,10 +60,13 @@ G.add_edge(2,6)
 G.add_edge(3,7)
 G.add_edge(3,8)
 G.add_edge(4,9)
+"""
 
-
+G = gen_sommet()
+        
 pos = hierarchy_pos(G,1)
 print(pos)
+
 nx.draw(G, pos=pos, with_labels=False)
 node_labels = nx.get_node_attributes(G,'weight')
 nx.draw_networkx_labels(G, pos, labels = node_labels)
