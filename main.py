3f1716736cf890e67ba5ae6c9c3ec9836d4c8613
alemahie <32846956+alemahie@users.noreply.github.com>
2018-10-30 20:07:43 +0100

    Add max_subtree function

--- main.py
+++ main.py
@@ -33,28 +33,16 @@ def gen_sommet():
 		\brief : cree un arbre de 10 a 15 sommets, dont chacun a un poids de -10 à 10
 		\param : None
 		\return : un arbre
-		\complex : 
 	"""
 	G=nx.Graph()
 	n_sommets = random.randint(10,15) #genere entre 10 et 15 sommets
 	for i in range(n_sommets):
-		G.add_node(i+1, weight = random.randint(-10,10)) #Cree un noeud aleatoire
+		G.add_node(i+1, weight = random.randint(-5,10)) #Cree un noeud aleatoire
 		if i != 0:
 			G.add_edge(random.randint(1, i), i+1) #Relie ce nouveau noeud (sauf le premier), aleatoirement parmi les precedents
 	return G
 	
-def gen_max(i):
-	voisins = list(G.neighbors(i))
-	print(i, end=' ')
-    
-	if i != 1:
-		print(voisins[1:])
-		for j in range(len(voisins)-1):
-			gen_max(voisins[j+1])
-	else:
-		print(voisins)
-		for j in voisins:
-			gen_max(j)
+	
 
 def delete_node(G,N):
 	"""
@@ -65,16 +53,71 @@ def delete_node(G,N):
 	"""
 	fils = list(G.neighbors(N))[1:]
 	for i in fils:
-		G.delete_node(i)
-	
+		G = delete_node(G, i)
+	G.remove_node(N)
 	return G
+	
+	
+
+def sous_somme(voisins, somme_des_fils, dec):
+	somme = 0
+	for i in voisins[dec:]:
+		somme += somme_des_fils[i]
+	return somme
+	
+	
+
+def max_subtree(i, node_labels, somme_des_fils, G):
+	voisins = list(G.neighbors(i))
+	if i != 1:
+		for j in range(len(voisins)-1):
+			G = max_subtree(voisins[j+1], node_labels, somme_des_fils, G)
+		if voisins[1:] == [] and node_labels[i] > 0:		#Si feuille > 0, ok
+			somme_des_fils[i] = node_labels[i]
+		elif voisins[1:] == [] and node_labels[i] <= 0:		#Si feuilles <= 0, bye bye
+			somme_des_fils[i] = 0
+			G = delete_node(G, i)
+		if voisins[1:] != [] and node_labels[i] <= 0:	 	#Si pas feuille < 0, check sous-arbre
+			somme = sous_somme(voisins, somme_des_fils, 1)
+			somme_des_fils[i] = somme
+			if somme + node_labels[i] > 0:					#Si noeud + fils > 0, ok
+				somme_des_fils[i] += node_labels[i]
+			else:											#Si noeud + fils <= 0, bye bye
+				somme_des_fils[i] = 0
+				G = delete_node(G, i)
+		elif voisins[1:] != [] and node_labels[i] > 0:		#Si pas feuille > 0, ok
+			somme = sous_somme(voisins, somme_des_fils, 1)
+			somme_des_fils[i] = somme + node_labels[i]
+	else:
+		for j in voisins:
+			G = max_subtree(j, node_labels, somme_des_fils, G)
+				
+		somme = sous_somme(voisins, somme_des_fils, 0)
+		somme_des_fils[i] = somme + node_labels[i]
+				
+		if somme_des_fils[i] > 0:		
+			return G
+		else:							#Aucun arbre max > 0
+			res = nx.Graph()
+			res.add_node(1, weight = 0)
+			return res
+	return G
+	
+	
 
 ##########################################################################################################
 
+G = gen_sommet()   
 
-G = gen_sommet()       
 pos = hierarchy_pos(G,1)
+nx.draw(G, pos=pos, with_labels=False)
+node_labels = nx.get_node_attributes(G,'weight')
+nx.draw_networkx_labels(G, pos, labels = node_labels) #affiche les poids a la place des noms
+plt.show()
 
+G = max_subtree(1, node_labels, {}, G)
+
+pos = hierarchy_pos(G,1)
 nx.draw(G, pos=pos, with_labels=False)
 node_labels = nx.get_node_attributes(G,'weight')
 nx.draw_networkx_labels(G, pos, labels = node_labels) #affiche les poids a la place des noms
